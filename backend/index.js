147f4e26b1b580241626992726729c585e6389b6
RehanImam <rehanmallick297@gmail.com>
2026-06-07 13:20:24 +0530

    adding render link in index file

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://job-portal-foryou.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
