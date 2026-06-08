1f2fcea4d88fcd8f5f41cd37c1445206be8e6a84
RehanImam <rehanmallick297@gmail.com>
2026-06-08 12:21:07 +0530

    remove slash

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://job-portal-w1gh.vercel.app/"],
+    origin: ["http://localhost:3000","https://job-portal-w1gh.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
